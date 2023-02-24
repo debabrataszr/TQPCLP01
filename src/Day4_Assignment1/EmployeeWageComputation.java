a19f364b83b6de6458ece6948e1eadb8e6999b5b
debabrata-97 <debabrataszr@gmail.com>
2023-02-24 10:13:51 +0530

    Check Employee is Present or Absent

--- src/Day4_Assignment1/EmployeeWageComputation.java
+++ src/Day4_Assignment1/EmployeeWageComputation.java
@@ -3,6 +3,16 @@ package Day4_Assignment1;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Problem");
-    }
+        final int FULL_TIME = 1;
+        int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME) {
+            System.out.println("Employee is Present");
 
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
 }
+
+
+
