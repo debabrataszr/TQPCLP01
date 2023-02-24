219395c36fc55c3e2d3d41d854a5cec4758854b7
debabrata-97 <debabrataszr@gmail.com>
2023-02-24 10:19:52 +0530

    Calculate Daily Employee Wage

--- src/Day4_Assignment1/EmployeeWageComputation.java
+++ src/Day4_Assignment1/EmployeeWageComputation.java
@@ -4,13 +4,17 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Problem");
         final int FULL_TIME = 1;
+        final int WAGE_PER_HR = 20;
         int empType = (int) (Math.random() * 100) % 2;
+        int workingHours = 0;
         if (empType == FULL_TIME) {
             System.out.println("Employee is Present");
-
+            workingHours = 8;
         } else {
             System.out.println("Employee is Absent");
         }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
 
