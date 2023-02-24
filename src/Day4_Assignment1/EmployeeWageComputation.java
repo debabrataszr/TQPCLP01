c74ab83f57411d5a25ba4b8ffe83b6a7733e2202
debabrata-97 <debabrataszr@gmail.com>
2023-02-24 10:05:38 +0530

    Welcome to Employee Wage Computation Problem

---
+++ src/Day4_Assignment1/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package Day4_Assignment1;
+
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Problem");
+    }
+
+}
